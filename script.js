0845436548024183876495ccb5c201b1ff55f023
TUNMBI <adelowotumbi1@gmail.com>
2022-09-25 16:00:55 +0200

    pushing my first javascript experience

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// multiplication
-let multiple1 = 3
-multiple1 *= 2
+// // multiplication
+// let multiple1 = 3
+// multiple1 *= 2
 
-document.getElementById('Ans').innerHTML = "<code> let muLtiple1=3 </code> <br> <code> multiple1 *=2 </code> <br> <code> " + multiple1;
+// document.getElementById('Ans').innerHTML = "<code> let muLtiple1=3 </code> <br> <code> multiple1 *=2 </code> <br> <code> " + multiple1;
 
-// exponetial
-let exponent = 5
-exponent **= 10
+// // exponetial
+// let exponent = 5
+// exponent **= 10
 
-document.getElementById('expo').innerHTML = "<code> let exponent =5 </code> <br> <code> exponent **=10 </code> <br> <code>" + exponent;
-
-
-// modulus
-let modulus = 5
-modulus %= 200
-
-document.getElementById('modulus1').innerHTML = "<code> let modulus = 5 </code> <br> <code> modulus %= 200 </code> <br> = " + modulus;
+// document.getElementById('expo').innerHTML = "<code> let exponent =5 </code> <br> <code> exponent **=10 </code> <br> <code>" + exponent;
 
 
+// // modulus
+// let modulus = 5
+// modulus %= 200
 
-// division
-let division = 5
-division /= 5
+// document.getElementById('modulus1').innerHTML = "<code> let modulus = 5 </code> <br> <code> modulus %= 200 </code> <br> = " + modulus;
 
-document.getElementById('divide').innerHTML = "<code> let division = 5</code> <br> <code> division /= 5 </code> <br>" + division;                           
+
+
+// // division
+// let division = 5
+// division /= 5
+
+// document.getElementById('divide').innerHTML = "<code> let division = 5</code> <br> <code> division /= 5 </code> <br>" + division;                           
 
 
 
