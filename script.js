4355cf7441c6566bd0a8cfbbb505781ff45df590
TUNMBI <adelowotumbi1@gmail.com>
2022-09-25 15:38:23 +0200

    mmy third day javascript class

---
+++ script.js
@@ -0,0 +1,76 @@
+// multiplication
+let multiple1 = 3
+multiple1 *= 2
+
+document.getElementById('Ans').innerHTML = "<code> let muLtiple1=3 </code> <br> <code> multiple1 *=2 </code> <br> <code> " + multiple1;
+
+// exponetial
+let exponent = 5
+exponent **= 10
+
+document.getElementById('expo').innerHTML = "<code> let exponent =5 </code> <br> <code> exponent **=10 </code> <br> <code>" + exponent;
+
+
+// modulus
+let modulus = 5
+modulus %= 200
+
+document.getElementById('modulus1').innerHTML = "<code> let modulus = 5 </code> <br> <code> modulus %= 200 </code> <br> = " + modulus;
+
+
+
+// division
+let division = 5
+division /= 5
+
+document.getElementById('divide').innerHTML = "<code> let division = 5</code> <br> <code> division /= 5 </code> <br>" + division;                           
+
+
+
+ 
+//    document.write("<p>Welcome to my third day javascript class</P>");
+// document.write("<p>My Third Day experience in Javascript Was Awesome though I guess Its Becoming more deeper and more Different.Third day lesson was on Javascript Operators..</p>");
+// document.write("<p> Javascript is fun to learn according to my research and I believe as times goes on and with a lot of practices i would Understand More better...</p>");
+// document.write("<p><h1>  </h1></p>");
+// assighmnt operators
+// document.write("<p><h1><u> Javascript Operators </u></h1></p>"); 
+// document.write("<p><h2> Assignment Operators </h2></p>");   
+// document.write("<p><h4>Multiplication Assignment Operators</h4></p>"); 
+// document.write("<p><h4>Exponential Assignment Operators:</h4></p>"); 
+// document.write("<p></p>"); 
+// console.log("Welcome to js class");
+// window.alert("Welcome To My Third-Day Javascript Experience");
+// document.getElementById('title').innerHTML="Assignment";
+// document.getElementById('title 1').innerHTML="My Third-Day Javascript Experience";
+// document.getElementById('experience').innerHTML="My Third Day experience in Javascript Was Awesome though I guess Its Becoming more deeper and more Different.Third day lesson was on Javascript Operators..Javascript is fun to learn according to my research and I believe as times goes on and with a lot of practices i would Understand More better...";
+// document.getElementById('topic').innerHTML="<h2><u>Javascript Operators </u></h2>";
+// document.getElementById('subtopic').innerHTML="<h3> Assignment Operators </h3>";
+// document.getElementById('multiple').innerHTML="<h4>Multiplication Assignment Operators:</h4>";
+// document.getElementById('exponent').innerHTML="<h4>Exponential Assignment Operators:</h4>";
+// document.getElementById('').innerHTML="";
+// document.getElementById('').innerHTML="";
+
+
+
+
+// let multiple1 = 3
+// multiple1 *= 2
+
+// document.getElementById('Ans').innerHTML ="<code> let muLtiple1=3 </code> <br> <code> multiple1 *=2 </code> <br> <code> " + multiple1;
+
+
+// let exponent = 5
+// exponent **= 10
+
+// document.getElementById('expo').innerHTML ="<code> let exponent =5 </code> <br> <code> exponent **=10 </code> <br> <code>" + exponent;
+
+
+
+
+
+
+
+
+
+
+ 
